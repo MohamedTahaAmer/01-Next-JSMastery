cfea4203e5e99d72630623cb2524ba930e623f28
MohamedTahaAmer <MohamedTahaaAmer@gmail.com>
2023-06-23 09:42:15 +0300

    added an under lay for the dropdown menu

--- components/Nav.jsx
+++ components/Nav.jsx
@@ -85,44 +85,52 @@ const Nav = () => {
       <div className="sm:hidden flex relative">
         {session?.user ? (
           <div className="flex">
-            <Image
-              src={session?.user.image}
-              width={37}
-              height={37}
-              className="rounded-full"
-              alt="profile"
-              onClick={() => setToggleDropdown((s) => !s)}
-            />
-
-            {toggleDropdown &&
-             (
-              <div className="absolute right-0 top-full mt-3 w-full p-5 rounded-lg bg-white min-w-[210px] flex flex-col gap-2 justify-end items-end">
-                <Link
-                  href="/profile"
-                  className="text-sm font-inter text-gray-700 hover:text-gray-500 font-medium"
-                  onClick={() => setToggleDropdown(false)}
-                >
-                  My Profile
-                </Link>
-                <Link
-                  href="/create-prompt"
-                  className="text-sm font-inter text-gray-700 hover:text-gray-500 font-medium"
-                  onClick={() => setToggleDropdown(false)}
-                >
-                  Create Prompt
-                </Link>
-                <button
-                  type="button"
-                  onClick={() => {
-                    setToggleDropdown(false);
-                    signOut();
-                  }}
-                  className="mt-5 w-full rounded-full border border-black bg-black py-1.5 px-5 text-white transition-all hover:bg-white hover:text-black text-center text-sm font-inter flex items-center justify-center"
-                >
-                  Sign Out
-                </button>
-              </div>
+            {toggleDropdown && (
+              <div
+                className="fixed top-0 left-0 w-full h-full z-10 bg-stone-200 bg-opacity-50"
+                onClick={() => setToggleDropdown(false)}
+              ></div>
             )}
+
+            <div className="relative z-20">
+              <Image
+                src={session?.user.image}
+                width={37}
+                height={37}
+                className="rounded-full"
+                alt="profile"
+                onClick={() => setToggleDropdown((s) => !s)}
+              />
+
+              {toggleDropdown && (
+                <div className="absolute  right-0 top-full mt-3 w-full p-5 rounded-lg bg-white min-w-[210px] flex flex-col gap-2 justify-end items-end">
+                  <Link
+                    href="/profile"
+                    className="text-sm font-inter text-gray-700 hover:text-gray-500 font-medium"
+                    onClick={() => setToggleDropdown(false)}
+                  >
+                    My Profile
+                  </Link>
+                  <Link
+                    href="/create-prompt"
+                    className="text-sm font-inter text-gray-700 hover:text-gray-500 font-medium"
+                    onClick={() => setToggleDropdown(false)}
+                  >
+                    Create Prompt
+                  </Link>
+                  <button
+                    type="button"
+                    onClick={() => {
+                      setToggleDropdown(false);
+                      signOut();
+                    }}
+                    className="mt-5 w-full rounded-full border border-black bg-black py-1.5 px-5 text-white transition-all hover:bg-white hover:text-black text-center text-sm font-inter flex items-center justify-center"
+                  >
+                    Sign Out
+                  </button>
+                </div>
+              )}
+            </div>
           </div>
         ) : (
           <>
