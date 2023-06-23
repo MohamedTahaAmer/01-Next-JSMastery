05b72e491ee72e0992c57b68f210d3eadb347b83
MohamedTahaAmer <MohamedTahaaAmer@gmail.com>
2023-06-23 06:34:38 +0300

    Why we used the <br> in the <h1>

--- app/page.jsx
+++ app/page.jsx
@@ -4,6 +4,9 @@ const Home = () => (
   <section className='w-full flex-center flex-col'>
     <h1 className='head_text text-center'>
       Discover & Share
+
+    {/* this br is added to break the header in two lines in the big screens, 
+    in small screen if we have two lines, and each line will broken because of the small width, then we will have about 4 lines which take the hole screen, so we removed the br to allow the h1 to be one line, to broken in smallest horizental space possible*/}
       <br className='max-md:hidden' />
       {/* 1- 'max-md' apply hidden to smaller than md, unlike 'md' which means apply hidden to bigger than md 
             - with max, the big is ok, apply to the small
